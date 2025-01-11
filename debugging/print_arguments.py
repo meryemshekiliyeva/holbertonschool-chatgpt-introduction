7550f153ce8a0e2f176d8efd19c0866a8e2a1321
Maryam <meryemshekiliyeva@gmail.com>
2025-01-11 22:18:13 +0400

    Update print_arguments.py

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-# Loop through arguments starting from index 1 to skip the script name
-for argument in sys.argv[1:]:
-    print(argument)
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
